da1f8e9e5df0d33df7055f18dfd5887201b1beea
清野 <dgtvydtrnr@privaterelay.appleid.com>
2026-08-22 16:51:21 +0800

    update deb packages

---
+++ update.sh
@@ -0,0 +1,21 @@
+#!/bin/zsh
+# 清理旧索引
+rm -f Packages Packages.bz2 Packages.gz
+# 扫描deb生成包列表
+dpkg-scanpackages debs /dev/null > Packages
+# 生成压缩索引（Cydia/Sileo必需）
+bzip2 -k Packages
+gzip -k Packages
+
+# ========== Release 文件内容，自行修改下面信息 ==========
+cat > Release << EOF
+Origin: Tony
+Label: 个人源
+Suite: stable
+Codename: ios
+Architectures: iphoneos-arm64 iphoneos-arm
+Components: main
+Description: 在这里填写仓库简介
+EOF
+
+echo "✅ 源索引更新完成！可以上传GitHub"
